c76d1184df90f1a2568b206103c8f3d076d78e25
Dawid Dęby <dawid.deby@gmail.com>
2021-02-05 16:22:26 +0100

    a lot of good stuf. persistance test and protector test

--- mastery_persistence/config/dev.exs
+++ mastery_persistence/config/dev.exs
@@ -2,8 +2,6 @@ use Mix.Config
 
 config :mastery_persistence, MasteryPersistence.Repo,
   username: "postgres",
-  # database: "postgres",
   password: "postgres",
-  # hostname: "localhost"
   database: "mastery_dev",
   hostname: "localhost"
--- mastery_persistence/config/test.exs
+++ mastery_persistence/config/test.exs
@@ -2,9 +2,7 @@ use Mix.Config
 
 config :mastery_persistence, MasteryPersistence.Repo,
   username: "postgres",
-  # database: "postgres",
   password: "postgres",
-  # hostname: "localhost",
   database: "mastery_dev",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
